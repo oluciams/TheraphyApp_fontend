9327af2c107283836c2113cb3ef12f55050655e5
oluciams <oluciams84@gmail.com>
2023-11-22 16:56:22 -0500

    add routes and list component to the teraphy folder

---
+++ src/app/teraphy/teraphy-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+
+// localhost:4200/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'list', component: ListPageComponent },
+      { path: '**', redirectTo: 'list' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TeraphyRoutingModule {}
--- src/app/teraphy/teraphy.module.ts
+++ src/app/teraphy/teraphy.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
+import { TeraphyRoutingModule } from './teraphy-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    ListPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    TeraphyRoutingModule
   ]
 })
 export class TeraphyModule { }
