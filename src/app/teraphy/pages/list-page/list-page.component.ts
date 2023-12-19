fb1110976cce8974240b9f937724030bf2c08b91
oluciams <oluciams84@gmail.com>
2023-12-19 11:29:58 -0500

    add patients service

--- src/app/teraphy/pages/list-page/list-page.component.ts
+++ src/app/teraphy/pages/list-page/list-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Patient } from '../../interfaces/patient.interface';
+import { PatientsService } from '../../services/patients.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: [],
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public patients: Patient[] = [];
+
+  constructor(private patientsService: PatientsService) { }
+
+  ngOnInit(): void {
+    this.patientsService.getPatients()
+      .subscribe(patients => this.patients = patients);
+
+  }
 }
---
+++ src/app/teraphy/services/patients.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Patient } from '../interfaces/patient.interface';
+import { environments } from 'src/environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class PatientsService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPatients(): Observable<Patient[]> {
+   return this.http.get<Patient[]>(`${this.baseUrl}/pacients`)
+   // return this.http.get<Patient[]>();
+
+  }
+
+}
