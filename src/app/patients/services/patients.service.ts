d092e93688fc6b312aeaabfd5f0565599425a014
oluciams <oluciams84@gmail.com>
2023-12-27 14:29:06 -0500

    adds addPatient, updatePatient and deletepatientById methods in patientsService

--- src/app/patients/services/patients.service.ts
+++ src/app/patients/services/patients.service.ts
@@ -1,36 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Patient } from '../interfaces/patient.interface';
 import { environments } from 'src/environments/environments';
 import { Gender } from '../interfaces/gender.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PatientsService {
-
   private baseUrl: string = environments.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPatients(): Observable<Patient[]> {
-   return this.http.get<Patient[]>(`${this.baseUrl}/pacients`)
+    return this.http.get<Patient[]>(`${this.baseUrl}/pacients`);
   }
 
-  getPatientById(id: string): Observable<Patient|undefined> {
-    return this.http.get<Patient>(`${this.baseUrl}/pacients/${id}`)
-      .pipe(
-        catchError(error =>  of(undefined))
-      );
+  getPatientById( id: string ): Observable<Patient | undefined> {
+    return this.http
+      .get<Patient>(`${this.baseUrl}/pacients/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 
-  getSuggestions(query: string): Observable<Patient[]> {
-    return this.http.get<Patient[]>(`${this.baseUrl}/pacients/?q=${ query }&_limit=3`)
-
+  getSuggestions( query: string ): Observable<Patient[]> {
+    return this.http.get<Patient[]>(
+      `${this.baseUrl}/pacients/?q=${query}&_limit=3`
+    );
   }
 
   getPatientsGenders(): Observable<Gender[]> {
-
-    return this.http.get<Gender[]>(`${this.baseUrl}/genders`)
+    return this.http.get<Gender[]>(`${this.baseUrl}/genders`);
   }
 
+  addPatient( patient: Patient ): Observable<Patient> {
+    return this.http.post<Patient>(`${this.baseUrl}/pacients`, patient);
+  }
+
+  updatePatient( patient: Patient ): Observable<Patient> {
+    if (!patient.id) throw Error('Patient id is required');
+    return this.http.patch<Patient>( `${this.baseUrl}/pacients/${patient.id}`, patient );
+  }
+
+  //Si quiero reemplazar toda la data utilizo el metodo update
+  // updatePatientPut(patient: Patient): Observable<Patient> {
+  //   return this.http.put<Patient>(`${this.baseUrl}/pacients/${ patient.id }`, patient);
+  // }
+
+  deletePatientById( id: string ): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/pacients/${ id }`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      );
+  }
 }
+
