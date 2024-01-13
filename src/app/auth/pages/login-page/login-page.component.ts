ba1d3b6914e434c49776850caa7d0ff79842d404
oluciams <oluciams84@gmail.com>
2024-01-12 19:18:03 -0500

    adds authService and methods onLogout and login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  
 })
 export class LoginPageComponent {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  onLogin(): void {
+    this.authService.login('yady.cuellar@email.com', '123456')
+      .subscribe(user => {
+
+        this.router.navigate(['/'])
+
+      });
+
+  }
 }
