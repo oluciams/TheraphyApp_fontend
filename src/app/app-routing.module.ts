e9f3acf626c28a7f01ab3bbc6c8abcec4d504348
oluciams <oluciams84@gmail.com>
2024-01-16 18:58:30 -0500

    add publicGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [PublicGuard],
+    canMatch: [PublicGuard],
   },
   {
     path: 'patients',
-    loadChildren: () => import('./patients/patients.module').then((m) => m.PatientsModule),
+    loadChildren: () =>
+      import('./patients/patients.module').then((m) => m.PatientsModule),
     canActivate: [AuthGuard],
-    canMatch: [AuthGuard]
+    canMatch: [AuthGuard],
   },
   {
     path: '404',
@@ -21,7 +25,7 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'patients',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
